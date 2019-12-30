0ba1204d66f05852f56c360d0aa424dfffe3f068
Paul Tobin <tobster111@gmail.com>
2019-12-30 22:41:25 +0000

    Completed the UPDATE Challenges

--- CRUD.sql
+++ CRUD.sql
@@ -101,5 +101,11 @@ CREATE TABLE cats
   -- Updating Data
   UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
 
+  UPDATE cats SET age=14 WHERE name='Misty';
 
-  UPDATE cats SET age=14 WHERE name='Misty'; 
+  -- Update Challenges 
+  UPDATE cats SET name='Jack' WHERE name='Jackson';
+
+  UPDATE cats SET breed='British Shorthair' WHERE name='Ringo';
+
+  UPDATE cats SET age=12 WHERE breed='Maine Coon';
