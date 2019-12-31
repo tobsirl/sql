e195e9e371c248c0fcc13858c77c334ae0fbf75b
Paul Tobin <tobster111@gmail.com>
2019-12-31 12:22:00 +0000

    Crud challenges - finihed challeges with updates and deletes

--- CRUD-Challenges.sql
+++ CRUD-Challenges.sql
@@ -26,3 +26,43 @@ CREATE TABLE shirts
     ('polo shirt', 'red', 'M', 5),
     ('tank top', 'white', 'S', 200),
     ('tank top', 'blue', 'M', 15);
+
+  INSERT INTO shirts
+    (article, color, shirt_size, last_worn)
+  VALUES
+    ('polo shirt', 'purple', 'M', 50);
+
+  SELECT article, color
+  FROM shirts;
+
+  SELECT article, color, shirt_size, last_worn
+  FROM shirts
+  WHERE shirt_size='M';
+
+  SELECT *
+  FROM shirts
+  WHERE article='polo shirt';
+
+  UPDATE shirts SET shirt_size='L' WHERE article='polo shirt';
+
+  SELECT *
+  FROM shirts
+  WHERE last_worn=15;
+
+  UPDATE shirts SET last_worn=0 WHERE last_worn=15;
+
+  SELECT *
+  FROM shirts
+  WHERE color='white';
+
+  UPDATE shirts SET shirt_size='XS', color='off white' WHERE color='white';
+
+  SELECT * FROM shirts WHERE last_worn=200;
+
+  DELETE FROM shirts WHERE last_worn=200;
+
+  DELETE FROM shirts WHERE article='tank top';
+
+  DELETE FROM shirts;
+
+  DROP TABLE shirts;
