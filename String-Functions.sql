ffda8cd8b2610740b9e7142a04893c35ce39d7f6
Paul Tobin <tobster111@gmail.com>
2020-01-03 18:33:10 +0000

    Examples of using REVERSE

--- String-Functions.sql
+++ String-Functions.sql
@@ -127,3 +127,15 @@ FROM books;
 SELECT
   SUBSTRING(REPLACE(title, 'e', '3'), 1, 10) AS 'weird string'
 FROM books;
+
+-- Using REVERSE
+
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
