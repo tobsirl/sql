c052a458045a573dea41fc1eb8e83421a0b63cb9
Paul Tobin <tobster111@gmail.com>
2020-01-04 17:02:00 +0000

    Example of using UPPER and LOWER

--- String-Functions.sql
+++ String-Functions.sql
@@ -146,3 +146,14 @@ SELECT CHAR_LENGTH('Hello World');
 SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
  
 SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+-- Using UPPER and LOWER
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
