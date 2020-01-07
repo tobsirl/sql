19677b9cbb82232c50968888c9ee239d2943bca0
Paul Tobin <tobster111@gmail.com>
2020-01-07 19:32:36 +0000

    Exercise 5 and 6 complete

--- 8-Refining-Our-Selections/Selections-Exercises.sql
+++ 8-Refining-Our-Selections/Selections-Exercises.sql
@@ -15,4 +15,18 @@ SELECT CONCAT
 FROM books ORDER BY released_year DESC LIMIT 3;
 
 -- Exercise 4
-SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+SELECT title, author_lname
+FROM books
+WHERE author_lname LIKE '% %';
+
+-- Exercise 5
+SELECT title, released_year, stock_quantity
+FROM books
+ORDER BY stock_quantity
+LIMIT 3;
+
+-- Exercise 6
+SELECT title
+, author_lname
+FROM books
+ORDER BY author_lname, title;
