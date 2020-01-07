b81c34a083c2f3d55d0eea136458067dc25cd470
Paul Tobin <tobster111@gmail.com>
2020-01-07 19:23:43 +0000

    Exercise 3 and 4 complete

--- 8-Refining-Our-Selections/Selections-Exercises.sql
+++ 8-Refining-Our-Selections/Selections-Exercises.sql
@@ -8,3 +8,11 @@ SELECT title, pages
 FROM books
 ORDER BY pages DESC
 LIMIT 1;
+
+-- Exercise 3
+SELECT CONCAT
+(title,' - ',  released_year) AS 'summary' 
+FROM books ORDER BY released_year DESC LIMIT 3;
+
+-- Exercise 4
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
