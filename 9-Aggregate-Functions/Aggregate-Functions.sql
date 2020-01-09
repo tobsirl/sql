7cc09ef5dbeb071767dc94bd6050e683be0d3693
Paul Tobin <tobster111@gmail.com>
2020-01-09 17:20:48 +0000

    Aggregate Functions - COUNT

---
+++ 9-Aggregate-Functions/Aggregate-Functions.sql
@@ -0,0 +1,23 @@
+-- Count Function
+SELECT COUNT(*)
+FROM books;
+
+SELECT COUNT(author_fname)
+FROM books;
+
+SELECT COUNT(DISTINCT author_fname)
+FROM books;
+
+SELECT COUNT(DISTINCT author_lname)
+FROM books;
+
+SELECT COUNT(DISTINCT author_lname, author_fname
+) FROM books;
+
+SELECT title
+FROM books
+WHERE title LIKE '%the%';
+
+SELECT COUNT(*)
+FROM books
+WHERE title LIKE '%the%';
