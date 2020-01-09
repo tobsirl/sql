e27259d9d8a6d3f32d8ec16bd5f06c7927120d7f
Paul Tobin <tobster111@gmail.com>
2020-01-09 20:00:07 +0000

    Aggregate Functions - Problem with Min and Max

--- 9-Aggregate-Functions/Aggregate-Functions.sql
+++ 9-Aggregate-Functions/Aggregate-Functions.sql
@@ -79,3 +79,32 @@ FROM books;
 
 SELECT MAX(pages), title
 FROM books;
+
+-- A Problem with Min and Max
+SELECT *
+FROM books
+WHERE pages = (SELECT Min(pages)
+FROM books);
+
+SELECT title, pages
+FROM books
+WHERE pages = (SELECT Max(pages)
+FROM books);
+
+SELECT title, pages
+FROM books
+WHERE pages = (SELECT Min(pages)
+FROM books);
+
+SELECT *
+FROM books
+ORDER BY pages ASC LIMIT 1;
+ 
+SELECT title
+, pages FROM books 
+ORDER BY pages ASC LIMIT 1;
+
+SELECT *
+FROM books
+ORDER BY pages DESC
+LIMIT 1;
