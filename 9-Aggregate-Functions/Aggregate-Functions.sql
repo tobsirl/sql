2fc26a5befa5fbb2f3eddfae32d1d606bff069fa
Paul Tobin <tobster111@gmail.com>
2020-01-09 17:50:18 +0000

    Aggregate Functions - GROUP BY

--- 9-Aggregate-Functions/Aggregate-Functions.sql
+++ 9-Aggregate-Functions/Aggregate-Functions.sql
@@ -21,3 +21,42 @@ WHERE title LIKE '%the%';
 SELECT COUNT(*)
 FROM books
 WHERE title LIKE '%the%';
+
+-- GROUP BY
+SELECT title, author_lname
+FROM books;
+
+SELECT title, author_lname
+FROM books
+GROUP BY author_lname;
+
+SELECT author_lname, COUNT(*)
+FROM books
+GROUP BY author_lname;
+
+
+SELECT title, author_fname, author_lname
+FROM books;
+
+SELECT title, author_fname, author_lname
+FROM books
+GROUP BY author_lname;
+
+SELECT author_fname, author_lname, COUNT(*)
+FROM books
+GROUP BY author_lname;
+
+SELECT author_fname, author_lname, COUNT(*)
+FROM books
+GROUP BY author_lname, author_fname;
+
+SELECT released_year
+FROM books;
+
+SELECT released_year, COUNT(*)
+FROM books
+GROUP BY released_year;
+
+SELECT CONCAT('In ', released_year, ' ', COUNT(*), ' book(s) released') AS year
+FROM books
+GROUP BY released_year;
