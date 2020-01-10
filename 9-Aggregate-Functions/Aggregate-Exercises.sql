864fb452788042a876b53b3451b4cdec17f7712f
Paul Tobin <tobster111@gmail.com>
2020-01-10 11:18:59 +0000

    Aggregate Functions - Exercise 3 + 4 complete

--- 9-Aggregate-Functions/Aggregate-Exercises.sql
+++ 9-Aggregate-Functions/Aggregate-Exercises.sql
@@ -6,3 +6,12 @@ FROM books;
 SELECT released_year AS Year, COUNT(released_year) AS 'Num of books released'
 FROM books
 GROUP BY released_year;
+
+-- Exercise 3 Print out the total number of books in stock
+SELECT SUM(stock_quantity) AS 'Total Number of books'
+FROM books;
+
+-- Exercise 4 Find the average released_year for each author
+SELECT CONCAT(author_lname, ' ', author_fname) AS 'Name', AVG(released_year)
+FROM books
+GROUP BY author_lname, author_fname;
