6bf41fff8677c3a4105a26647b8b3f1d8ae35c53
Paul Tobin <tobster111@gmail.com>
2020-01-10 10:47:26 +0000

    Aggregate Functions - SUM function

--- 9-Aggregate-Functions/Aggregate-Functions.sql
+++ 9-Aggregate-Functions/Aggregate-Functions.sql
@@ -133,3 +133,26 @@ SELECT
 FROM books
 GROUP BY author_lname,
          author_fname;
+
+--  Sum Function
+SELECT SUM(pages)
+FROM books;
+
+SELECT SUM(released_year)
+FROM books;
+
+SELECT author_fname,
+  author_lname,
+  Sum(pages)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+
+SELECT author_fname,
+  author_lname,
+  Sum(released_year)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
