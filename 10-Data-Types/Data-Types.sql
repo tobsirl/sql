ce54e6e33abf63ccd772d38b489243da62a654f5
Paul Tobin <tobster111@gmail.com>
2020-01-12 11:38:31 +0000

    Data Types - DECIMAL

--- 10-Data-Types/Data-Types.sql
+++ 10-Data-Types/Data-Types.sql
@@ -30,3 +30,32 @@ VALUES
 
 SELECT *
 FROM dogs;
+
+-- DECIMAL
+CREATE TABLE items
+(
+  price DECIMAL(5,2)
+);
+
+INSERT INTO items
+  (price)
+VALUES(7);
+
+INSERT INTO items
+  (price)
+VALUES(7987654);
+
+INSERT INTO items
+  (price)
+VALUES(34.88);
+
+INSERT INTO items
+  (price)
+VALUES(298.9999);
+
+INSERT INTO items
+  (price)
+VALUES(1.9999);
+
+SELECT *
+FROM items;
