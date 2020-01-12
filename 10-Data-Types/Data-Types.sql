df759e79324c825b07566993d1b8eca028495f9a
Paul Tobin <tobster111@gmail.com>
2020-01-12 19:34:17 +0000

    Data Types - Formatting Dates

--- 10-Data-Types/Data-Types.sql
+++ 10-Data-Types/Data-Types.sql
@@ -109,3 +109,52 @@ VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
 
 SELECT *
 FROM people;
+
+-- Formatting Dates
+SELECT name, birthdate
+FROM people;
+
+SELECT name, DAY(birthdate)
+FROM people;
+
+SELECT name, birthdate, DAY(birthdate)
+FROM people;
+
+SELECT name, birthdate, DAYNAME(birthdate)
+FROM people;
+
+SELECT name, birthdate, DAYOFWEEK(birthdate)
+FROM people;
+
+SELECT name, birthdate, DAYOFYEAR(birthdate)
+FROM people;
+
+SELECT name, birthtime, DAYOFYEAR(birthtime)
+FROM people;
+
+SELECT name, birthdt, DAYOFYEAR(birthdt)
+FROM people;
+
+SELECT name, birthdt, MONTH(birthdt)
+FROM people;
+
+SELECT name, birthdt, MONTHNAME(birthdt)
+FROM people;
+
+SELECT name, birthtime, HOUR(birthtime)
+FROM people;
+
+SELECT name, birthtime, MINUTE(birthtime)
+FROM people;
+
+SELECT CONCAT(MONTHNAME(birthdate), ' ', DAY(birthdate), ' ', YEAR(birthdate))
+FROM people;
+
+SELECT DATE_FORMAT(birthdt, 'Was born on a %W')
+FROM people;
+
+SELECT DATE_FORMAT(birthdt, '%m/%d/%Y')
+FROM people;
+
+SELECT DATE_FORMAT(birthdt, '%m/%d/%Y at %h:%i')
+FROM people;
