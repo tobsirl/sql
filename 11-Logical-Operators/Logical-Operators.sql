d0c1af53776ad746e75c48de7c176cdd412b4037
Paul Tobin <tobster111@gmail.com>
2020-01-14 11:52:31 +0000

    Logical Operators - Not Equal

---
+++ 11-Logical-Operators/Logical-Operators.sql
@@ -0,0 +1,19 @@
+-- Not Equal
+SELECT title
+FROM books
+WHERE released_year = 2017;
+
+SELECT title
+FROM books
+WHERE released_year != 2017;
+
+SELECT title, author_lname
+FROM books;
+
+SELECT title, author_lname
+FROM books
+WHERE author_lname = 'Harris';
+
+SELECT title, author_lname
+FROM books
+WHERE author_lname != 'Harris';
