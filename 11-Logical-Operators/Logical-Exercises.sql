0ba537002fcd7be117abcaa7bc51f6d1413d0e72
Paul Tobin <tobster111@gmail.com>
2020-01-15 16:31:37 +0000

    Logical Operator - Exercises 3 and 4

--- 11-Logical-Operators/Logical-Exercises.sql
+++ 11-Logical-Operators/Logical-Exercises.sql
@@ -16,3 +16,13 @@ IN
 SELECT title, released_year
 FROM books
 WHERE released_year < 1980;
+
+-- Exercise 3 - Select All Books Written By Eggers Or Chabon
+SELECT title, author_lname, released_year
+FROM books
+WHERE author_lname='Eggers' OR author_lname='Chabon';
+
+-- Exercise 4 - Select All Books Written By Lahiri, Published after 2000
+SELECT title, author_lname, released_year
+FROM books
+WHERE author_lname='Lahiri' AND released_year > 2000;
