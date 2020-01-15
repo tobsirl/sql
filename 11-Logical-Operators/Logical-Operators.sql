4128a032e226f14d52f24c31812107c58a808cad
Paul Tobin <tobster111@gmail.com>
2020-01-15 16:05:30 +0000

    Logical Operator - Case Statements

--- 11-Logical-Operators/Logical-Operators.sql
+++ 11-Logical-Operators/Logical-Operators.sql
@@ -286,3 +286,44 @@ FROM books
 WHERE released_year >= 2000 AND
   released_year % 2 != 0
 ORDER BY released_year;
+
+-- Case Statements
+SELECT title, released_year,
+  CASE 
+         WHEN released_year >= 2000 THEN 'Modern Lit'
+         ELSE '20th Century Lit'
+       END AS GENRE
+FROM books;
+
+SELECT title, stock_quantity,
+  CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+
+SELECT title,
+  CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+
+SELECT title, stock_quantity,
+  CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+        ELSE '****'
+    END AS STOCK
+FROM books;
+
+SELECT title, stock_quantity,
+  CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
