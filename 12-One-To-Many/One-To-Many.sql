41050580bad04e45547ff21bd041b319e9bb7797
Paul Tobin <tobster111@gmail.com>
2020-01-16 14:48:29 +0000

    Using a cross join

--- 12-One-To-Many/One-To-Many.sql
+++ 12-One-To-Many/One-To-Many.sql
@@ -42,3 +42,19 @@ CREATE TABLE customers
       ('2014/12/12', 800.67, 2),
       ('2015/01/03', 12.50, 2),
       ('1999/04/11', 450.25, 5);
+
+-- Cross Joins
+-- Finding Orders Placed By George: 2 Step Process
+
+SELECT id FROM customers WHERE last_name='George';
+SELECT * FROM orders WHERE customer_id = 1;
+-- Finding Orders Placed By George: Using a subquery
+
+SELECT * FROM orders WHERE customer_id =
+    (
+        SELECT id FROM customers
+        WHERE last_name='George'
+    );
+-- Cross Join Craziness
+
+SELECT * FROM customers, orders;
