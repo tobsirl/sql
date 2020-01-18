ee46822492484a5860ddc625f711af4f9c281bd3
Paul Tobin <tobster111@gmail.com>
2020-01-18 10:47:58 +0000

    One To Many - Exercise 4 IFNULL()

--- 12-One-To-Many/One-To-Many-Exercise.sql
+++ 12-One-To-Many/One-To-Many-Exercise.sql
@@ -35,15 +35,23 @@ CREATE TABLE students
     (2, 'De Montaigne and The Art of The Essay', 98),
     (4, 'Borges and Magical Realism', 89);
 
--- Exercise 2 
+  -- Exercise 2 
   SELECT first_name, title, grade
   FROM students
     INNER JOIN papers
     ON students.id = papers.student_id
   ORDER BY grade DESC;
 
--- Exercise 3 - Left Join
-SELECT first_name, title, grade
-FROM students
-LEFT JOIN papers
-ON students.id = papers.student_id;
+  -- Exercise 3 - Left Join
+  SELECT first_name, title, grade
+  FROM students
+    LEFT JOIN papers
+    ON students.id = papers.student_id;
+
+  -- Exercise 4 - Using IFNULL()
+  SELECT first_name,
+    IFNULL(title, 'MISSING'),
+    IFNULL(grade, 0)
+  FROM students
+    LEFT JOIN papers
+    ON students.id = papers.student_id;
