8c8170b6b5c99310f8e98e27d835ba394817f411
Paul Tobin <tobster111@gmail.com>
2020-01-19 12:33:08 +0000

    Many To Many - Insert data into reviewers and series

--- 13-Many-To-Many/Many-To-Many.sql
+++ 13-Many-To-Many/Many-To-Many.sql
@@ -20,3 +20,32 @@ CREATE TABLE reviewers
   genre VARCHAR
     (100)
 );
+
+    INSERT INTO series
+      (title, released_year, genre)
+    VALUES
+      ('Archer', 2009, 'Animation'),
+      ('Arrested Development', 2003, 'Comedy'),
+      ("Bob's Burgers", 2011, 'Animation'),
+      ('Bojack Horseman', 2014, 'Animation'),
+      ("Breaking Bad", 2008, 'Drama'),
+      ('Curb Your Enthusiasm', 2000, 'Comedy'),
+      ("Fargo", 2014, 'Drama'),
+      ('Freaks and Geeks', 1999, 'Comedy'),
+      ('General Hospital', 1963, 'Drama'),
+      ('Halt and Catch Fire', 2014, 'Drama'),
+      ('Malcolm In The Middle', 2000, 'Comedy'),
+      ('Pushing Daisies', 2007, 'Comedy'),
+      ('Seinfeld', 1989, 'Comedy'),
+      ('Stranger Things', 2016, 'Drama');
+
+    INSERT INTO reviewers
+      (first_name, last_name)
+    VALUES
+      ('Thomas', 'Stoneman'),
+      ('Wyatt', 'Skaggs'),
+      ('Kimbra', 'Masters'),
+      ('Domingo', 'Cortes'),
+      ('Colt', 'Steele'),
+      ('Pinkie', 'Petit'),
+      ('Marlon', 'Crafford');
