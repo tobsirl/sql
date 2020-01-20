5c394a89e73c18c409c0d4b484b4bd65ba30cca9
Paul Tobin <tobster111@gmail.com>
2020-01-20 16:30:22 +0000

    TV Joins - Exercise 4 complete

--- 13-Many-To-Many/TV-Joins-Challenges.sql
+++ 13-Many-To-Many/TV-Joins-Challenges.sql
@@ -16,3 +16,10 @@ ORDER BY avg_rating;
 SELECT first_name, last_name, rating
 FROM reviewers
   INNER JOIN reviews ON reviewers.id = reviews.reviewer_id;
+
+-- Exercise 4
+SELECT title AS unreviewed_series
+FROM series
+  LEFT JOIN reviews
+  ON series.id = reviews.series_id
+WHERE rating IS NULL;
