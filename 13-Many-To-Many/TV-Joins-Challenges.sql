328564b8c6d577cc5a480295f01e6f14e4bbf5fa
Paul Tobin <tobster111@gmail.com>
2020-01-21 17:03:07 +0000

    TV Joins - Exercise 6 Reviewer Stats

--- 13-Many-To-Many/TV-Joins-Challenges.sql
+++ 13-Many-To-Many/TV-Joins-Challenges.sql
@@ -30,3 +30,47 @@ SELECT genre,
 FROM series
   JOIN reviews ON series.id = reviews.series_id
 GROUP BY genre;
+
+-- Exercise 6 - Reviewer Stats
+SELECT first_name,
+  last_name,
+  COUNT(reviewer_id) AS COUNT,
+  IFNULL(MIN(rating), 0) AS MIN,
+  IFNULL(MAX(rating), 0) AS MAX,
+  IFNULL(AVG(rating), 0) AS AVG,
+  CASE 
+        WHEN COUNT(rating) >= 1 THEN 'ACTIVE'
+        ELSE 'INACTIVE'
+    END AS STATUS
+FROM reviewers
+  LEFT JOIN reviews ON reviewers.id = reviews.reviewer_id
+GROUP BY reviewer_id;
+
+-- Solution using an IF statement
+SELECT first_name,
+  last_name,
+  COUNT(reviewer_id) AS COUNT,
+  IFNULL(MIN(rating), 0) AS MIN,
+  IFNULL(MAX(rating), 0) AS MAX,
+  IFNULL(AVG(rating), 0) AS AVG, 
+IF(COUNT(rating) >= 1, 'ACTIVE', 'INACTIVE') AS STATUS
+FROM reviewers
+  LEFT JOIN reviews ON reviewers.id = reviews.reviewer_id
+GROUP BY reviewer_id;
+
+-- Reviewer Stats With POWER USERS 
+SELECT first_name,
+  last_name,
+  Count(rating)                    AS COUNT,
+  Ifnull(Min(rating), 0)           AS MIN,
+  Ifnull(Max(rating), 0)           AS MAX,
+  Round(Ifnull(Avg(rating), 0), 2) AS AVG,
+  CASE 
+         WHEN Count(rating) >= 10 THEN 'POWER USER' 
+         WHEN Count(rating) > 0 THEN 'ACTIVE' 
+         ELSE 'INACTIVE' 
+       end                              AS STATUS
+FROM reviewers
+  LEFT JOIN reviews
+  ON reviewers.id = reviews.reviewer_id
+GROUP  BY reviewers.id; 
