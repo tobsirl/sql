d826aea5b2602148212ab30f594ece1fa006c99d
Paul Tobin <tobster111@gmail.com>
2020-01-21 18:23:57 +0000

    TV Joins - Exercise 7 three tables

--- 13-Many-To-Many/TV-Joins-Challenges.sql
+++ 13-Many-To-Many/TV-Joins-Challenges.sql
@@ -73,4 +73,11 @@ SELECT first_name,
 FROM reviewers
   LEFT JOIN reviews
   ON reviewers.id = reviews.reviewer_id
-GROUP  BY reviewers.id; 
+GROUP  BY reviewers.id;
+
+-- Exercise 7 - 3 tables
+SELECT title, rating, CONCAT(first_name, ' ', last_name) AS reviewer
+FROM reviewers
+  INNER JOIN reviews ON reviewers.id = reviews.reviewer_id
+  INNER JOIN series ON series.id = reviews.series_id
+ORDER BY title;
