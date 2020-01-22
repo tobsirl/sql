3c0f3474dd906875d67bbcd98732801fa99aa2c2
Paul Tobin <tobster111@gmail.com>
2020-01-22 13:45:39 +0000

    Instagram Database - Photos Schema

--- 14-Instagram-Database/Instagram-DB.sql
+++ 14-Instagram-Database/Instagram-DB.sql
@@ -8,3 +8,18 @@ CREATE TABLE users
     created_at TIMESTAMP DEFAULT NOW
   ()
 );
+
+  -- IG Clone Photos Schema
+  CREATE TABLE photos
+  (
+    id INTEGER
+    AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR
+    (255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW
+    (),
+    FOREIGN KEY
+    (user_id) REFERENCES users
+    (id)
+);
