1c31a44336f70d13a7b274006a3fe803b1b91d0a
Paul Tobin <tobster111@gmail.com>
2020-01-24 18:03:34 +0000

    Instagram Questions - Exercise 3

--- 14-Instagram-Database/Instagram-Questions.sql
+++ 14-Instagram-Database/Instagram-Questions.sql
@@ -24,3 +24,10 @@ FROM users
 GROUP BY day
 ORDER BY total DESC
 LIMIT 2;
+
+-- Exercise 3 - Identify Inactive Users (users with no photos)
+SELECT username
+FROM users
+LEFT JOIN photos
+    ON users.id = photos.user_id
+WHERE photos.id IS NULL;
