7bf2ad66d410d13593291eee556a85908b11c7a1
Paul Tobin <tobster111@gmail.com>
2020-01-24 17:48:10 +0000

    Instagram Questions - Exercise 2

--- 14-Instagram-Database/Instagram-Questions.sql
+++ 14-Instagram-Database/Instagram-Questions.sql
@@ -1,5 +1,26 @@
+-- Exercise 1
 -- We want to reward our users who have been around the longest.  
 -- Find the 5 oldest users.
-SELECT * FROM users
+SELECT *
+FROM users
 ORDER BY created_at
 LIMIT 5;
+
+-- -- Exercise 2 - Most Popular Registration Date
+SELECT DATE_FORMAT
+(created_at, '%W') AS DAY,
+COUNT
+(DAYOFWEEK
+(created_at)) AS COUNT
+FROM users
+GROUP BY DATE_FORMAT
+(created_at, '%W')
+ORDER BY COUNT DESC;
+
+SELECT
+  DAYNAME(created_at) AS day,
+  COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
