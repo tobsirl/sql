a9ff0a9abe1709afbd6b81c07e344ef95c35dc82
Paul Tobin <tobster111@gmail.com>
2020-01-25 15:03:26 +0000

    Instagram - Exercise 7

--- 14-Instagram-Database/Instagram-Questions.sql
+++ 14-Instagram-Database/Instagram-Questions.sql
@@ -64,3 +64,16 @@ FROM photo_tags
 GROUP  BY tags.id
 ORDER  BY total DESC
 LIMIT  5; 
+
+-- Exercise 7 - Finding the bots - the users who have liked every single photo
+SELECT username
+, 
+       Count
+(*) AS num_likes 
+FROM   users 
+       INNER JOIN likes 
+               ON users.id = likes.user_id 
+GROUP  BY likes.user_id 
+HAVING num_likes =
+(SELECT Count(*)
+FROM photos); 
