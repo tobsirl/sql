57313aeb872394a74d5fcf258d65f39168080cf7
Paul Tobin <tobster111@gmail.com>
2020-01-27 14:47:34 +0000

    Insert hardcoded data in the database

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -13,11 +13,21 @@ const db = mysql.createConnection({
 // db.query('CREATE DATABASE join_us');
 // db.query('USE join_us');
 
-const sqlQuery = 'SELECT COUNT(*) AS total FROM users';
+// SELECTING DATA
+// const sqlQuery = 'SELECT COUNT(*) AS total FROM users';
 
-db.query(sqlQuery, (error, results, fields) => {
-  if (error) console.warn(error);
-  console.log(results[0].total);
+// db.query(sqlQuery, (error, results, fields) => {
+//   if (error) console.warn(error);
+//   console.log(results[0].total);
+// });
+
+
+// INSERTING DATA
+const q = 'INSERT INTO users (email) VALUES ("rusty_the_dog@gmail.com")';
+ 
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
 });
 
 db.end();
