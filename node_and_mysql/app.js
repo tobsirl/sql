5a43dd77c0616b544356475ef0ff73999257b5cb
Paul Tobin <tobster111@gmail.com>
2020-01-27 14:57:38 +0000

    Using faker to create faker accounts and inserting them into the database

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -23,11 +23,21 @@ const db = mysql.createConnection({
 
 
 // INSERTING DATA
-const q = 'INSERT INTO users (email) VALUES ("rusty_the_dog@gmail.com")';
+// const q = 'INSERT INTO users (email) VALUES ("rusty_the_dog@gmail.com")';
  
-connection.query(q, function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
+// db.query(q, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
+
+const person = {
+  email: faker.internet.email(),
+  created_at: faker.date.past()
+};
+
+db.query('INSERT INTO users SET ?', person, function(err, result) {
+if (err) throw err;
+console.log(result);
 });
 
 db.end();
