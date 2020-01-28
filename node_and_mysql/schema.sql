41710bdc797871f6cc84307a419968af64f272f5
Paul Tobin <tobster111@gmail.com>
2020-01-28 15:05:13 +0000

    Exercise 1 - Find Earliest Date A User Joined

--- node_and_mysql/schema.sql
+++ node_and_mysql/schema.sql
@@ -4,4 +4,13 @@ CREATE TABLE users
   created_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO users (email) VALUES('Katie34@yahoo.com'), ('Tunde@gmail.com');
+INSERT INTO users
+  (email)
+VALUES('Katie34@yahoo.com'),
+  ('Tunde@gmail.com');
+
+-- Exercise 1 - Find Earliest Date A User Joined
+SELECT DATE_FORMAT(created_at,  "%M %D %Y") AS earliest_date
+FROM users
+ORDER BY earliest_date
+LIMIT 1;
