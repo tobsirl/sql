5dde80809152da590b83292fbeea9788586bd0d6
Paul Tobin <tobster111@gmail.com>
2020-01-29 20:00:43 +0000

    Added two more routes /joke and /random

--- JoinUS/app.js
+++ JoinUS/app.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello from the web app!');
 });
 
+app.get('/joke', (req, res) => {
+  const joke =
+    'What do you call a dog that does magic tricks? A labracadabrador.';
+  res.send(joke);
+});
+
+app.get('/random', (req, res) => {
+  const num = Math.floor(Math.random() * 10 + 1);
+  res.send(`Your lucky number is ${num}`);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`);
 });
