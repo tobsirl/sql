a1db199174b601a018201dfcfcec6133593fcbeb
Paul Tobin <tobster111@gmail.com>
2020-01-30 12:15:31 +0000

    Requiring mysql and making the database connection

--- JoinUS/app.js
+++ JoinUS/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mysql = require('mysql');
 require('dotenv').config();
 
 const app = express();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+// db connection
+const db = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+});
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello from the web app!');
