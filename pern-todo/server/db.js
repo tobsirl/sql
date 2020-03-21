5576ce2d13d028dd2eb114aaccd3a8a98db5c4b7
Paul Tobin <tobster111@gmail.com>
2020-03-21 17:22:11 +0000

    database.sql for creating the database and todo table, dotenv for environment variables. db.js for the database connection

---
+++ pern-todo/server/db.js
@@ -0,0 +1,18 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const {
+  POST_USER,
+  POST_PASSWORD,
+  POST_HOST,
+  POST_PORT,
+  POST_DATABASE
+} = process.env;
+
+const pool = new Pool({
+  user: POST_USER,
+  password: POST_PASSWORD,
+  host: POST_HOST,
+  port: POST_PORT,
+  database: POST_DATABASE
+});
--- pern-todo/server/index.js
+++ pern-todo/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,12 +10,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({
-    status: success,
+    status: 'success',
     data: 'HI'
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
